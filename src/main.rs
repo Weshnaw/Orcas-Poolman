14d883fbd10955898b9f6f1abfdcb0f241b1725a
Brendon Fallquist <bfallquist@gmail.com>
2026-01-16 15:37:16 -0500

    add todo

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ async fn main() -> Result<(), Error> {
         let file = file?;
         let file = file.path();
 
-        reconcile_file(&file).await;
+        handle_file(&file).await;
     }
 
     info!("Initializing filament.json watcher");
@@ -64,7 +64,7 @@ async fn main() -> Result<(), Error> {
                         ModifyKind::Any | ModifyKind::Data(_) | ModifyKind::Other,
                     ) => {
                         for path in event.paths {
-                            reconcile_file(&path).await;
+                            handle_file(&path).await;
                         }
                     }
                     _ => {} // skip
@@ -103,7 +103,7 @@ fn parse_file(path: &PathBuf) -> Result<FilamentConfig, Error> {
     Ok(serde_json::from_reader(BufReader::new(File::open(path)?))?)
 }
 
-async fn reconcile_file(path: &PathBuf) {
+async fn handle_file(path: &PathBuf) {
     if path.extension().is_some_and(|ext| ext != "json") || path.is_dir() {
         debug!("invalid file: {:?}", path);
         return;
@@ -111,8 +111,35 @@ async fn reconcile_file(path: &PathBuf) {
 
     match parse_file(path) {
         Ok(config) => {
-            todo!("handle config: {:?}", config)
+            reconcile_config(config).await;
         }
         Err(e) => warn!("file parsing error: {:?}, err: {:?}", path, e),
     }
+}
+
+// Priority List:
+// Allow option to ignore OpenPrintTag values in case folks do not want to go through the hassle of adding OpenPrintTag to the extra fields, maybe in the future I will support other filament tags
+// OpenPrintTag value this should be from the manufacturer so these values should take precident (we do not touch these values)
+// Spoolman == Local (tie breaker is most recent last_modified unless local is specifically marked as 
+//                         spoolman_force_pull (forces the local to change with current spoolman) or 
+//                         spoolman_force_push (forces spoolman to update to the current local)) 
+//             (store changes in local file somewhere)
+// the file name should be: (Manufacturer) Material - Name @Printer // can overwrite this on a per printer basis 
+// Exta data is stored in orca_poolman in json format, it can also contain any overrides for OpenPrintTag data
+// We will only update Fields such as Extruder Temp once if they do not exist, but we rely on the orca_poolman for the reconcilation of bed/extruder temp
+// the data in orca_poolman will consist of a map of printers, and a overrides object for more generic data like min/max temp, usually OpenPrintTag overrides
+// for e.x. 
+// {
+//   'printers': { "Voron": {...}, "Prusa": {...}}
+//   'overrides': {...}
+// }
+// the orca slicer filament_notes will contain json data for the spoolman_id (optional str), printer_id (optional str),  spoolman_force_push/pull (optional bool)
+// spoolman_force_push/pull will be removed after reconcilation
+// if the printer_id cannot be determined (@... is not present, printer_id is not present, or compatible_printers) then reconcilation does not occur and an error message is populated in the notes
+// if an error field is present AT_ALL then we do not proceed with reconcilation
+// add reconcilation_status (updated_spoolman, updated_local, etc) to the filament_notes, this is ignored in terms of the actual spoolman_reconcilation
+// add dryrun (optional bool) which if present and true prevents the service from making any changes and outputs the desired changes to a new entry desired_spoolman, desired_local
+// last_modified just cause I don't want to deal with OS modified
+async fn reconcile_config(config: FilamentConfig) {
+    todo!("handle config: {:?}", config)
 }
